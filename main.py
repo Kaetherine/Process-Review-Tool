897e620aec9898b2ffcbd24a265f88115eeeabd9
Kuhle <DE117339@de.ema.ad.pwcinternal.com>
2023-05-27 11:15:04 +0200

    /

--- main.py
+++ main.py
@@ -168,4 +168,4 @@ def parse_data(contents, filename):
         return html.Div(['There was an error processing this file.'])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8080)
