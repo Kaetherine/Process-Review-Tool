d5e589bb033726241e34d8258043eda1ef11545e
Kaetherine <kuhle.katherine@googlemail.com>
2023-05-30 17:14:26 +0200

    /

--- application.py
+++ application.py
@@ -168,4 +168,4 @@ app.layout = dash.html.Div(
 #         return html.Div(['There was an error processing this file.'])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8080)
+    app.run(debug=True, port=8080)
